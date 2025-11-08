77d140cacce09baf27b504a04308d48ea722c76b
Mikhail Fokin <fokinms@gmail.com>
2025-11-09 02:29:32 +0400

    JRS 007 Modified compose.yaml, Dockerfile and build.gradle.kts. Deleted health-check.sh -> healthcheck changed from script to actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     /**
      * Spring boot starters
      */
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
